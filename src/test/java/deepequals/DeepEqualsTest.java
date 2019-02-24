7cd3ead17511fefa8581147be9aebc581506ccd1
grossvater <gitpatch@gmail.com>
2019-02-24 10:03:07 +0200

    add more tests

--- src/test/java/deepequals/DeepEqualsTest.java
+++ src/test/java/deepequals/DeepEqualsTest.java
@@ -500,6 +500,42 @@ public class DeepEqualsTest {
     }
 
     @Test
+    public void _20_ignoreException() {
+        class Foo {
+        	private int great;
+        	
+        	public Foo() {
+        	}
+        	
+            public int optional() { throw new RuntimeException(); }
+        }
+        
+        assertTrue(withOptions()
+                .ignore(Foo.class, "optional")
+                .deepEquals(Foo.class, new Foo(), new Foo()));
+    }
+
+    @Test
+    public void _21_ignoreNullField() {
+    	class Bar {
+    	}
+    	
+        class Foo {
+            private Bar zero;
+
+            public Foo(Bar zero) {
+                this.zero = zero;
+            }
+
+            public Bar zero() { return this.zero; }
+        }
+
+        assertTrue(withOptions()
+                .ignore(Foo.class, "zero")
+                .deepEquals(Foo.class, new Foo(new Bar()), new Foo(null)));
+    }
+    
+    @Test
     public void class_not() {
         class Foo {
             public String get() { return uniqueString(); }
